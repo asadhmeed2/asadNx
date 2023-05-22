36fc3e13d519789a68f291020262073a62ec6f69
asaad.hmeed <asaad.hmeed@runnableweb.com>
2023-05-22 09:48:12 +0300

    add header and layout to new react app

--- packages/new-react-app/src/modules/home/Home.tsx
+++ packages/new-react-app/src/modules/home/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Layout } from '../../shared/layout'
 
 import styles from './Home.module.scss'
 
 export const Home = () => {
     return (
-        <div>Home</div>
+        <Layout>Home</Layout>
     )
 }
---
+++ packages/new-react-app/src/shared/layout/Layout.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import { Header } from '../header'
+
+type Props = React.PropsWithChildren
+
+export const Layout = ({children}:Props) => {
+  return (
+    <div>
+        <Header/>
+        
+        <main>
+            {children}
+        </main>
+    </div>
+  )
+}
