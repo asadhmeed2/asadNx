30cc5097bdd4cda518d4c261a56d052c8d7874af
asaad.hmeed <asadhmeed2@gmail.com>
2023-05-23 23:14:09 +0300

    add some styles and data to ui

--- packages/new-react-app/src/modules/components/UserCard/InfoCard.tsx
+++ packages/new-react-app/src/modules/components/UserCard/InfoCard.tsx
@@ -1,24 +1,38 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Col, Container, Row } from 'react-bootstrap'
 
 
 import styles from './InfoCard.module.scss'
 
 const demoData = {
     name:'Asad Hmeed',
-    title:'Junior Frontend Developer'
+    title:'Junior Frontend Developer',
+    experience:1,
+    knowledgeIn:['react','react native','vue' , 'bootstrap','html','css','typescript']
 
 } 
 
 export const InfoCard = () => {
   return (
-    <Card>
-        <div className={styles.name}>
-            {demoData.name}
-        </div>
-        <div className={styles.name}>
-            {demoData.title}
-        </div>
+    <Card className={styles.card}>
+        <Container className={styles.container}>
+            <Row>
+                <Col sm="12" md="6" className={styles.name}>
+                <span className={styles.title}>{` Name :`}</span><span> {demoData.name}</span>
+                </Col>
+                <Col  sm="12" md="6"className={styles.name}>
+                <span className={styles.title}>{` Title :`} </span><span>{demoData.title}</span>
+                </Col>
+            </Row>
+            <Row>
+                <Col sm="12" md="6" className={styles.name}>
+                <span className={styles.title}>{` Experience :`}</span> <span>{demoData.experience} {` year ${demoData.experience >1 ?'\'s':""}`}</span>
+                </Col>
+                <Col  sm="12" md="6"className={styles.name}>
+                <span className={styles.title}>{` Knowledge in :`}</span> <span> {demoData.knowledgeIn.join(',')}</span>
+                </Col>
+            </Row>
+        </Container>
     </Card>
   )
 }
