611dab5744261a4fb8915f050de18db09458c155
asaad.hmeed <asadhmeed2@gmail.com>
2023-05-26 22:26:58 +0300

    add data and alfa look to proect card

--- packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
+++ packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 
+const demodata = [{id:'1', title:'Aba Hatuv  אבא חטוב', description:'', framwork:'react native',moreTecnologes:'',websiteUrl:'https://www.leptin4life.com/'},
+{id:'2', title:'yoyo delivery website', description:'', framwork:'nextjs',moreTecnologes:'',websiteUrl:'https://www.yoyo.delivery/'}
+,{id:'2', title:'yoyo delivery app', description:'', framwork:'expo react native ',moreTecnologes:'',websiteUrl:'https://www.yoyo.delivery/'}]
 
 export const ProjectsCardList = () => {
   return (
-    <div>Projects that i take part of :</div>
+    <div className='overflow-scroll d-flex ps-1'>
+    {
+      demodata.map((item)=>{
+
+       return <Card className='me-1'>
+                  <div className="">
+                    <span>{item.title}</span>
+                  </div>
+
+                  <div className="">
+                    <span>{item.framwork}</span>
+                  </div>
+
+
+                  <div className="">
+                    <a href={item.websiteUrl}>{item.websiteUrl}</a>
+                  </div>
+                </Card>
+      })
+    }
+    </div>
   )
 }
