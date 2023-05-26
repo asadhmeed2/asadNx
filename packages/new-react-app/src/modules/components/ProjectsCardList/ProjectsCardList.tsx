ab792ca61843eb8b6ce63f1355e9e61228ee1efc
asaad.hmeed <asadhmeed2@gmail.com>
2023-05-26 22:35:23 +0300

    add scss module for ProjectsCardList

--- packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
+++ packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
+import cn from 'classnames'
+
+
+import styles from './ProjectsCardList.module.scss'
 
 const demodata = [{id:'1', title:'Aba Hatuv  אבא חטוב', description:'', framwork:'react native',moreTecnologes:'',websiteUrl:'https://www.leptin4life.com/'},
 {id:'2', title:'yoyo delivery website', description:'', framwork:'nextjs',moreTecnologes:'',websiteUrl:'https://www.yoyo.delivery/'}
@@ -8,11 +12,11 @@ const demodata = [{id:'1', title:'Aba Hatuv  אבא חטוב', description:'', f
 
 export const ProjectsCardList = () => {
   return (
-    <div className='overflow-scroll d-flex ps-1'>
+    <div className={cn('d-flex ps-1 justify-content-center',styles.list)}>
     {
       demodata.map((item)=>{
 
-       return <Card className='me-1'>
+       return <Card className='me-1 p-2'>
                   <div className="">
                     <span>{item.title}</span>
                   </div>
