1b0cae283699b83e96d1ad456266b643aed521fa
asaad.hmeed <asadhmeed2@gmail.com>
2023-05-27 13:14:49 +0300

    add bootstrap icons

--- packages/new-react-app/src/modules/home/Home.tsx
+++ packages/new-react-app/src/modules/home/Home.tsx
@@ -7,6 +7,9 @@ import styles from './Home.module.scss'
 export const Home = () => {
     return (
         <Layout>
+            <div className="">
+            <i className="bi bi-file-earmark-person"></i>
+            </div>
             <InfoCard/>
 
             <div className="mt-2 px-4 ">
