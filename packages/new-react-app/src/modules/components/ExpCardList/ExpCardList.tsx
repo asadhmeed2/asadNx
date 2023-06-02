ec3fc616cb32c30ce57a1ebc0cc1f285dda6a8f9
asaad.hmeed <asadhmeed2@gmail.com>
2023-06-02 11:48:04 +0300

    new-react-app :: add spinner to expCardList and projetsCardList

--- packages/new-react-app/src/modules/components/ExpCardList/ExpCardList.tsx
+++ packages/new-react-app/src/modules/components/ExpCardList/ExpCardList.tsx
@@ -1,53 +1,64 @@
-import { UserExperience } from '@asadnx/shared-ts'
-import axios from 'axios'
-import { AppEnv } from '../../../env'
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+
+import { Col, Container, Row, Spinner } from 'react-bootstrap'
+
+import axios from 'axios'
+
+import { UserExperience } from '@asadnx/shared-ts'
+
+import { AppEnv } from '../../../env'
 
 
 export const ExpCardList = () => {
 
   const [experienceList , setExperienceList] = useState<UserExperience[]>([])
-
+  const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     
-  (async()=>{
-    try{
+    (async()=>{
+      try{
+        setLoading(true)
 
-      const res = await axios.get<UserExperience[]>(`${AppEnv.baseUrl}/userInfo/experience`)
+        const res = await axios.get<UserExperience[]>(`${AppEnv.baseUrl}/userInfo/experience`)
+  
+        setExperienceList(res.data)
+  
+      }catch(err){
+        console.error(err);
+      }finally{
+        setLoading(false)
+      }
 
-      setExperienceList(res.data)
-
-    }catch(err){
-      console.error(err);
-    }
-  })()
-    
-  }, [])
+    })()
+      
+    }, [])
   
 
 
   return (
-        
-    <Container  className='p-3 background-color'>
-        <Row className='mt-1 d-flex text-color justify-content-center'>
-       {experienceList.map(item =>{
-           return <Col sm="12" md="5" className='ms-1 ' key={item.id}>
-            <div className="fs-4">     
-                <span>{item.title}</span>
-            </div>
-            <span>{item.subTitle}</span>
-            <br />
-            <span>{item.description}</span>
-            <br />
-            <span >technolgies : <span className='text-success'>
-                {item.technolgies.join(', ')}
-            </span>
-            </span>
-        </Col>
-       })} 
-       </Row>
-    </Container>
+      <>
+        {!loading && 
+          <Container  className='p-3 background-color'>
+              <Row className='mt-1 d-flex text-color justify-content-center'>
+                {experienceList.map(item =>{
+                    return <Col sm="12" md="5" className='ms-1 ' key={item.id}>
+                      <div className="fs-4">     
+                          <span>{item.title}</span>
+                      </div>
+                      <span>{item.subTitle}</span>
+                      <br />
+                      <span>{item.description}</span>
+                      <br />
+                      <span >technolgies : <span className='text-success'>
+                          {item.technolgies.join(', ')}
+                      </span>
+                      </span>
+                  </Col>
+                })} 
+              </Row>
+          </Container>}
+          {loading && <Spinner variant='white'/>}
+    </>
   )
 }
--- packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
+++ packages/new-react-app/src/modules/components/ProjectsCardList/ProjectsCardList.tsx
@@ -1,22 +1,47 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { Card, Spinner } from 'react-bootstrap'
 
 import cn from 'classnames'
 
+import axios from 'axios'
+
+import { UserProject } from '@asadnx/shared-ts'
+
+import { AppEnv } from '../../../env'
 
 import styles from './ProjectsCardList.module.scss'
 
-const demodata = [{id:'1', title:'Aba Hatuv  אבא חטוב', description:'', framwork:'react native',moreTecnologes:'',websiteUrl:'https://www.leptin4life.com/'},
-{id:'2', title:'yoyo delivery website', description:'', framwork:'nextjs',moreTecnologes:'',websiteUrl:'https://www.yoyo.delivery/'}
-,{id:'3', title:'yoyo delivery app', description:'', framwork:'expo react native ',moreTecnologes:'',websiteUrl:'https://www.yoyo.delivery/'}]
 
 export const ProjectsCardList = () => {
-  return (
-    
-    <div className={cn('d-flex ps-1 justify-content-center',styles.list)}>
-    {
-      demodata.map((item)=>{
 
+  const [projects,setProjects] =useState<UserProject[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+
+  useEffect(() => {
+    
+    (async()=>{
+      try{
+        setLoading(true)
+        const res = await axios.get<UserProject[]>(`${AppEnv.baseUrl}/userInfo/projects`)
+  
+        setProjects(res.data);
+  
+      }catch(err){
+        console.error(err);
+      }finally{
+        setLoading(false);
+      }
+    })()
+      
+    }, [])
+
+  return (
+    <>
+    {!loading && <div className={cn('d-flex ps-1 justify-content-center',styles.list)}>
+    {
+      projects.map((item)=>{
+        
        return <Card className='me-1 mt-1 p-2 background-color text-color' key={item.id}>
                   <div className="">
                     <span>{item.title}</span>
@@ -33,6 +58,9 @@ export const ProjectsCardList = () => {
                 </Card>
       })
     }
-    </div>
+    </div>}
+
+    {loading && <Spinner variant='white'/>}
+        </>
   )
 }
