db4af71873976e7d374e82609c9058874c7aa89c
asaad.hmeed <asadhmeed2@gmail.com>
2023-06-03 17:18:52 +0300

    add userInfo input

--- packages/admin/src/modules/form/UserInfo/UserInfoForm.tsx
+++ packages/admin/src/modules/form/UserInfo/UserInfoForm.tsx
@@ -12,13 +12,19 @@ import styles from './UserInfoForm.module.scss'
 
 
 const schema = Yup.object({
-[EUserInfoFormNames.NAME]: Yup.string().required('error')
+[EUserInfoFormNames.NAME]: Yup.string().required('enter your name'),
+[EUserInfoFormNames.TITLE]: Yup.string().required('enter your title'),
+[EUserInfoFormNames.EXP]: Yup.string().matches( /^\d{0,2}$/,'enter a number'),
+[EUserInfoFormNames.LINKEDIN_URL]: Yup.string().matches(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+'enter a valid url ')
 
 })
 
 const initValues : Partial<UserInfo> = {
-    [EUserInfoFormNames.NAME]: undefined
-    
+    [EUserInfoFormNames.NAME]: undefined,
+    [EUserInfoFormNames.TITLE]: undefined,
+    [EUserInfoFormNames.EXP]: undefined,
+    [EUserInfoFormNames.LINKEDIN_URL]: undefined   
 }
 
 export const UserInfoForm = () => {
@@ -30,11 +36,37 @@ export const UserInfoForm = () => {
                 validationSchema={schema}
         >
             <Form>
+                {/* name */}
                 <div className={styles.input} id={EUserInfoFormNames.NAME}>
-                    <label htmlFor={EUserInfoFormNames.NAME}>User Info :</label>
+                    <span>User Name :</span>
                     <Field name={EUserInfoFormNames.NAME} component={AppInput}/>
-                    <ErrorMessage name={EUserInfoFormNames.NAME} />
                 </div>
+
+                <ErrorMessage name={EUserInfoFormNames.NAME} />
+
+                {/* title */}
+                <div className={styles.input} id={EUserInfoFormNames.TITLE}>
+                    <span >User Title :</span>
+                    <Field name={EUserInfoFormNames.TITLE} component={AppInput}/>
+                </div>
+
+                <ErrorMessage name={EUserInfoFormNames.TITLE} />
+
+                {/* experience */}
+                <div className={styles.input} id={EUserInfoFormNames.EXP}>
+                    <span >User Experience :</span>
+                    <Field name={EUserInfoFormNames.EXP} component={AppInput}/>  
+                </div>
+
+                <ErrorMessage name={EUserInfoFormNames.EXP} />
+
+                {/* linkedin url */}
+                <div className={styles.input} id={EUserInfoFormNames.LINKEDIN_URL}>
+                    <span >User Linkedin Url :</span>
+                    <Field name={EUserInfoFormNames.LINKEDIN_URL} component={AppInput}/>
+                </div>
+
+                <ErrorMessage name={EUserInfoFormNames.LINKEDIN_URL} />
 
                 <div className="">
                   <Button type='submit'>submit</Button>
--- packages/admin/src/modules/home/Home.tsx
+++ packages/admin/src/modules/home/Home.tsx
@@ -4,22 +4,22 @@ import { UserInfoForm } from '../form'
 
 export const Home = () => {
   return (
-    <div><Tabs
-    defaultActiveKey="userInfo"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="userInfo" title="Info">
-        <UserInfoForm/>
-      </Tab>
-      <Tab eventKey="UserExperience" title="Experience">
-        Tab content for Profile
-      </Tab>
-      <Tab eventKey="UserProjects" title="Projects" >
-        Tab content for Contact
-      </Tab>
-        
-        
-        </Tabs></div>
+    <div>
+        <Tabs
+            defaultActiveKey="userInfo"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+        >
+            <Tab eventKey="userInfo" title="Info" className='p-2'>
+                <UserInfoForm/>
+            </Tab>
+            <Tab eventKey="UserExperience" title="Experience">
+                Tab content for Profile
+            </Tab>
+            <Tab eventKey="UserProjects" title="Projects" >
+                Tab content for Contact
+            </Tab>  
+        </Tabs>
+    </div>
   )
 }
