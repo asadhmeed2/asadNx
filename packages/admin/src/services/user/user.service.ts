bb53ac9185df830dfac5ddada6da5669a5589158
asaad.hmeed <asadhmeed2@gmail.com>
2023-06-04 22:36:33 +0300

    admin :: add updateUserInfo to userService

--- packages/admin/src/services/user/user.service.ts
+++ packages/admin/src/services/user/user.service.ts
@@ -1,5 +1,11 @@
+import { UserInfo } from "@asadnx/shared-ts";
+
 class UserService {
    
+    updateUserInfo(userInfo:UserInfo){
+        console.log('updateUserInfo ',userInfo);
+    }
+
 }
 
 export const userService = new UserService();
