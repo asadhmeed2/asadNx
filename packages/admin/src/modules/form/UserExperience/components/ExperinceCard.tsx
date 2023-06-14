5d935de1154c69b62ab6a292ad6b25ba0cab0200
asaad.hmeed <asadhmeed2@gmail.com>
2023-06-14 23:36:48 +0300

    admin :: show user experience in ExperinceCard

--- packages/admin/src/modules/form/UserExperience/components/ExperinceCard.tsx
+++ packages/admin/src/modules/form/UserExperience/components/ExperinceCard.tsx
@@ -1,5 +1,6 @@
 import { UserExperience } from '@asadnx/shared-ts'
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 type Props ={
   userExperince: UserExperience
@@ -7,6 +8,22 @@ type Props ={
 
 export const ExperinceCard = ({userExperince}:Props) => {
   return (
-    <div>{userExperince.title}</div>
+    <Card>
+      <div className="">
+        <span>{userExperince.title}</span>
+      </div>
+
+      <div className="">
+        <span>{userExperince.subTitle}</span>
+      </div>
+
+      <div className="">
+       <span>{userExperince.description}</span>
+      </div>
+
+      <div className="">
+       <span>{userExperince.technolgies.join(',')}</span>
+      </div>
+    </Card>
   )
 }
