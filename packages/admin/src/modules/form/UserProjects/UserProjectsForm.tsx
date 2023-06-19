b1dceecec913cb08f1f30c4c6a0292bf8e14c2e4
newasad <asadhmeed2@gmail.com>
2023-06-19 23:38:07 +0300

    admin add userProject title input

--- packages/admin/src/modules/form/UserProjects/UserProjectsForm.tsx
+++ packages/admin/src/modules/form/UserProjects/UserProjectsForm.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useMemo } from 'react';
+
+import { EUserProjectFormNames, UserProject } from '@asadnx/shared-ts';
+import { ErrorMessage, Field, Formik } from 'formik';
+import { AppInput } from '../../../shared';
+import { Button, Form } from 'react-bootstrap';
+
+import * as Yup from 'yup';
+
+import cn from 'classnames';
+
+import styles from './UserProjectForm.module.scss';
+
+const schema = Yup.object({
+  [EUserProjectFormNames.TITLE]: Yup.string().required('Title is required'),
+});
 
 export const UserProjectsForm = () => {
+  const initValues: Partial<UserProject> = useMemo(
+    () => ({
+      [EUserProjectFormNames.TITLE]: undefined,
+    }),
+    []
+  );
+
   return (
-    <div>UserProjects</div>
-  )
-}
+    <div>
+      <Formik
+        initialValues={initValues}
+        onSubmit={(values, formikHelpers) => {
+          console.log(values);
+        }}
+        validationSchema={schema}
+        enableReinitialize
+      >
+        <Form>
+          {/* name */}
+          <div className={cn(styles.input)} id={EUserProjectFormNames.TITLE}>
+            <span className={styles.lable}>User Name :</span>
+            <Field name={EUserProjectFormNames.TITLE} component={AppInput} />
+          </div>
+
+          <div className={styles.error}>
+            <ErrorMessage name={EUserProjectFormNames.TITLE} />
+          </div>
+
+          <div className="text-center mt-5">
+            <Button type="submit">submit</Button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
