6d6390476889dbb966669be903948f1c576278fc
newasad <asadhmeed2@gmail.com>
2023-06-29 00:49:32 +0300

    asad-backend :: add mongoos to app.module

--- packages/asad-backend/src/app/app.module.ts
+++ packages/asad-backend/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserInfoModule } from '../userInfo/userInfo.module';
 
-
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserInfoModule],
-  
+  imports: [UserInfoModule, MongooseModule.forRoot('mongodb://localhost/nest')],
 })
 export class AppModule {}
