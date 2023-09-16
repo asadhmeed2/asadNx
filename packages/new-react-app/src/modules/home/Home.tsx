679f03bbdd628c2df1dc8330acd25861754d678d
newasad <asadhmeed2@gmail.com>
2023-09-16 22:17:05 +0300

    ..

--- packages/new-react-app/src/modules/home/Home.tsx
+++ packages/new-react-app/src/modules/home/Home.tsx
@@ -19,47 +19,57 @@ export const Home = () => {
           className={cn('z-0 position-absolute', styles.bgImage)}
         />
         <div
-          className={cn('pt-5 d-flex align-items-center w-100', styles.bgImage)}
+          className={cn(
+            'pt-5 d-flex align-items-center justify-content-center w-100 flex-1'
+          )}
         >
           <Tabs
             defaultActiveKey="info"
             id="uncontrolled-tab-example"
             className={cn(
-              'mb-3 mt-5 text-color d-flex flex-column  position-relative  z-1'
+              ' text-color d-flex flex-column  position-relative  z-1'
             )}
           >
             <Tab
               eventKey="info"
               title="Info"
-              className="position-relative  z-1 text-white w-100 h-100 "
+              className={cn('position-relative  z-1 text-white  h-100 ')}
             >
-              <InfoCard />
+              <div className={styles.tab}>
+                <InfoCard />
+              </div>
             </Tab>
             <Tab
               eventKey="exp"
               title="Experience"
-              className=" position-relative z-1 w-100 h-100 "
+              className={cn(' position-relative z-1 h-100 ', styles.content)}
             >
-              <h2 className="text-center text-color">Experience</h2>
-              <ExpCardList />
+              <div className={styles.tab}>
+                <h2 className="text-center text-color">Experience</h2>
+                <ExpCardList />
+              </div>
             </Tab>
             <Tab
               eventKey="projects"
               title="Projects"
               className="position-relative  z-1"
             >
-              <h2 className="text-center text-color">
-                projects that i take part of{' '}
-              </h2>
-              <ProjectsCardList />
+              <div className={styles.tab}>
+                <h2 className="text-center text-color">
+                  projects that i take part of{' '}
+                </h2>
+                <ProjectsCardList />
+              </div>
             </Tab>
 
             <Tab
               eventKey="play"
               title="Play Ground"
-              className="position-relative  z-1"
+              className={cn('position-relative  z-1', styles.content)}
             >
-              <h2 className="text-center text-color">play ground </h2>
+              <div className={styles.tab}>
+                <h2 className="text-center text-color">play ground </h2>
+              </div>
             </Tab>
           </Tabs>
         </div>
