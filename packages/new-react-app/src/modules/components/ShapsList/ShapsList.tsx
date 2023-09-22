ec9088f47600baf120b5013660c863b26aab22f1
newasad <asadhmeed2@gmail.com>
2023-09-22 18:27:00 +0300

    web :: shapsList / add list of shaps

--- packages/new-react-app/src/modules/components/ShapsList/ShapsList.tsx
+++ packages/new-react-app/src/modules/components/ShapsList/ShapsList.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+const objects: ShapeType[] = [
+  {
+    width: 100,
+    height: 50,
+  },
+];
+
 export const ShapsList = () => {
-  return <div>ShapsList</div>;
+  return (
+    <div>
+      {objects.map((item, idx) => (
+        <div
+          key={idx}
+          style={{
+            width: item.width,
+            height: item.height,
+            border: '1px solid black',
+          }}
+        ></div>
+      ))}
+    </div>
+  );
 };
+
+type ShapeType = { width: number; height: number };
